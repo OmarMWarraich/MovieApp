62f86c5dea8f84aacab4417bb8339331d01abcba
Mohamed Saleh <mohamed20163858@gmail.com>
2022-09-15 04:42:32 +0200

    send and recieve data through involvement api

--- src/modules/reservationsPopUp.js
+++ src/modules/reservationsPopUp.js
@@ -1,5 +1,68 @@
 import '../modulesCSS/reservationsPopUP.css';
 
+const reservationCounter = (dataArray) => dataArray.length;
+
+const sendRequest = async (apiURL, data) => {
+  const url = apiURL;
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
+  });
+  return response.text();
+};
+// function that we used to get our unique id which so our id is kc8ECGRfLEBUCmn5ZAnc
+/*
+const getId = async () => {
+  const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+  });
+  const id = await response.text();
+  console.log(id);
+};
+*/
+const displayReservationData = (data) => {
+  const reservations = document.querySelector('.reservations-done');
+  const reservationsData = JSON.parse(data);
+  const numOfReservations = reservationCounter(reservationsData);
+  if (reservationsData.error) {
+    reservations.innerHTML = `
+    <h3>Reservations(0)</h3>
+    <p>No reservation has been done yet</p>
+    `;
+    return;
+  }
+  reservations.innerHTML = `
+  <h3>Reservations(${numOfReservations})</h3>
+  `;
+  for (let i = 0; i < numOfReservations; i += 1) {
+    const p = document.createElement('p');
+    p.textContent = `${reservationsData[i].date_start} - ${reservationsData[i].date_end} by  ${reservationsData[i].username}`;
+    reservations.appendChild(p);
+  }
+};
+const getinvolveAPI = async (id) => {
+  const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/kc8ECGRfLEBUCmn5ZAnc/reservations?item_id=${id}`;
+  const response = await fetch(url);
+  const data = await response.text();
+  displayReservationData(data);
+};
 const displayInfo = (data) => {
   const showInfo = document.querySelector('.show-info');
   showInfo.innerHTML = `
@@ -20,35 +83,47 @@ const displayInfo = (data) => {
     </div>
   </div>
   <div class="reservations-done"></div>
-  <div class="add-reservation"></div>
+  <form action="" method="get">
+  <h3>Add a reservation</h3>
+  <div class="name">
+      <label for="name"></label>
+      <input type="text" name="name" id="name" placeholder="Your Name" required>
+  </div>
+  <div class="start-date">
+      <label for="startDate"></label>
+      <input type="date" name="startDate" id="startDate" required>
+  </div>
+  <div class="end-date">
+      <label for="endDate"></label>
+      <input type="date" name="endDate" id="endDate" required>
+  </div>
+  <button type="submit">Reserve</button>
+</form>
   `;
-  console.log(data);
+  getinvolveAPI(data.id);
+  const reserveButton = document.querySelector('form button');
+  reserveButton.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const name = document.querySelector('form .name input');
+    const startDate = document.querySelector('form .start-date input');
+    const endDate = document.querySelector('form .end-date input');
+    const reservationData = {
+      item_id: data.id,
+      username: name.value,
+      date_start: startDate.value,
+      date_end: endDate.value,
+    };
+    const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/kc8ECGRfLEBUCmn5ZAnc/reservations/';
+    await sendRequest(url, reservationData);
+    getinvolveAPI(data.id);
+  });
 };
-
 const getShowInfo = async (id) => {
   const url = `https://api.tvmaze.com/shows/${id}`;
   const response = await fetch(url);
   const data = await response.json();
   displayInfo(data);
 };
-/*
-const getinvolveAPI = async () => {
-  const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
-  const response = await fetch(url, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-  });
-  const data = await response.json();
-  console.log(data);
-};
-*/
 const ReservationsPopUP = (id) => {
   const popUP = document.createElement('section');
   const elements = document.querySelectorAll('body>*');
